228e396e2ba4ffd96904aadeb7619e18528b605d
Einars Vilnis <vilniseinars@gmail.com>
2017-11-08 22:19:22 +0000

    initial commit

---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(delete-selection-mode nil)
+ '(package-selected-packages
+   (quote
+    (git-gutter-fringe org-bullets org-mime org-plus-contrib company-c-headers company-math company-auctex evil-leader company-quickhelp company ag magit counsel powerline zenburn-theme persp-projectile perspective direx wrap-region multiple-cursors kpm-list ace-window undo-tree which-key helm-projectile helm use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
