cd027c2cb57742209e4dee87f0653a137535dbce
Einars Vilnis <vilniseinars@gmail.com>
2017-11-11 17:10:34 +0000

    Updated couple of files

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(delete-selection-mode nil)
  '(package-selected-packages
    (quote
-    (git-gutter-fringe org-bullets org-mime org-plus-contrib company-c-headers company-math company-auctex evil-leader company-quickhelp company ag magit counsel powerline zenburn-theme persp-projectile perspective direx wrap-region multiple-cursors kpm-list ace-window undo-tree which-key helm-projectile helm use-package))))
+    (php-mode tide org-tree-slide xref-js2 ob-coffee skewer-mode js2-refactor company-tern tern color-identifiers-mode all-the-icons neotree web-mode emmet-mode rainbow-delimiters highlight-parentheses elfeed-org elfeed-goodies company-cmake rjsx-mode git-gutter-fringe org-bullets org-mime org-plus-contrib company-c-headers company-math company-auctex evil-leader company-quickhelp company ag magit counsel powerline zenburn-theme persp-projectile perspective direx wrap-region multiple-cursors kpm-list ace-window undo-tree which-key helm-projectile helm use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
